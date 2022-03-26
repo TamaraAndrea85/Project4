e98dfc319b9d9c0e9339a32c1fe2369add647b68
tamaraandrea <tamara18_1985@msn.com>
2022-03-26 13:36:05 -0400

    got registration working on front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
+import Register from "./components/Users/Register/Register";
 import "./index.js";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
+      <Route path="/register" element={<Register />}></Route>
     </Routes>
   );
 }
